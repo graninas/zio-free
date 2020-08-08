f5415b326b355cbe44b8fca19d888ac5d4f31f35
Alexander Granin <graninas@gmail.com>
2020-08-08 09:52:53 +0700

    App improved.

--- app/Main.hs
+++ app/Main.hs
@@ -14,18 +14,32 @@ import ZIO.Effects.Effect.Language as L
 
 app :: ZIO Int
 app = do
-  line <- runEffect $ runIO P.getLine
+  line <- runEffect $ runIO $ pure "App methods finished."
 
-  eRes <- runEffect $ runIO $ do
+  eRes1 <- runEffect $ runIO $ do
+    threadDelay $ 1000 * 1000
+    P.putStrLn "Downloading 1..."
     _ :: Either SomeException String <- try $ Proc.readCreateProcess (Proc.shell "wget https://gist.github.com/graninas/01565065c18c01e88a5ebcbfbb96e397") ""
+    P.putStrLn "Finished downloading 1."
+    threadDelay $ 1000 * 500
     try $ P.readFile "01565065c18c01e88a5ebcbfbb96e397"
+
+  eRes2 <- runEffect $ runIO $ do
+    threadDelay $ 1000 * 500
+    P.putStrLn "Downloading 2..."
+    _ :: Either SomeException String <- try $ Proc.readCreateProcess (Proc.shell "wget https://gist.github.com/graninas/c7e0a603f3a22c7e85daa4599bf92525") ""
+    P.putStrLn "Finished downloading 2."
+    threadDelay $ 1000 * 1000
+    try $ P.readFile "c7e0a603f3a22c7e85daa4599bf92525"
 
   runEffect $ runIO $ do
     P.putStrLn line
-    let (content, size) = case eRes of
-          Left (err :: SomeException) -> (show err, 0)
-          Right content -> (content, length content)
-    P.putStrLn content
+    let (content1, content2, size) = case (eRes1, eRes2) of
+          (Left (err1 :: SomeException), _) -> (show err1, "", 0)
+          (_, Left (err2 :: SomeException)) -> ("", show err2, 0)
+          (Right content1, Right content2) -> (content1, content2, length content1 + length content2)
+    -- P.putStrLn content1
+    -- P.putStrLn content2
     P.putStrLn $ show size
     pure size
 
@@ -33,7 +47,8 @@ app = do
 main :: IO ()
 main = do
   rt <- R.createZIORuntime
-  v1 <- R.runZIOSync rt app
+  -- v1 <- R.runZIOSync rt app
+  -- P.putStrLn $ show v1
   R.Delayed var2 <- R.runZIOAsync rt app
   v2 <- takeMVar var2
-  P.putStrLn $ show $ v1 + v2
+  P.putStrLn $ show v2
