05ed385086d195d3af592b5a19623a5e578203d6
Alexander Granin <graninas@gmail.com>
2020-08-23 21:05:46 +0700

    Sample apps reorganized.

--- app/Main.hs
+++ app/Main.hs
@@ -15,11 +15,75 @@ import qualified ZIO.Effects.Effect.Language as L
 data Dummy = Dummy deriving (Show, Typeable)
 instance Exception Dummy
 
-wgetGist :: String -> L.AsyncEffect (T.Async String)
-wgetGist gist = L.evalIO $ do
+wgetGistIO :: String -> IO String
+wgetGistIO gist = do
   Proc.readCreateProcess (Proc.shell ("wget https://gist.github.com/graninas/" <> gist)) ""
   P.readFile gist
-  throwIO Dummy
+
+wgetGistIO' :: String -> IO (Either SomeException String)
+wgetGistIO' gist = do
+  Proc.readCreateProcess (Proc.shell ("wget https://gist.github.com/graninas/" <> gist)) ""
+  try $ P.readFile gist
+
+wgetGist :: String -> L.AsyncEffect (T.Async String)
+wgetGist gist = L.evalIO $ wgetGistIO gist
+
+wgetGist' :: String -> L.AsyncEffect (T.Async (Either SomeException String))
+wgetGist' gist = L.evalIO $ wgetGistIO' gist
+
+-- Sample 1
+
+
+appLogic :: L.AsyncEffect ()
+appLogic = do
+  line' :: T.Async String <- L.evalIO $ pure "App methods finished."
+
+  eRes1' :: T.Async (Either SomeException String) <- L.evalIO $ do
+    threadDelay $ 1000 * 1000 * 2
+    P.putStrLn "Downloading 1..."
+    void $ wgetGistIO' "01565065c18c01e88a5ebcbfbb96e397"
+    P.putStrLn "Finished downloading 1."
+    threadDelay $ 1000 * 1000
+    content <- try $ P.readFile "01565065c18c01e88a5ebcbfbb96e397"
+    P.putStrLn "File 1 read."
+    pure content
+
+  eRes2' :: T.Async (Either SomeException String) <- L.evalIO $ do
+    threadDelay $ 1000 * 500 * 2
+    P.putStrLn "Downloading 2..."
+    void $ wgetGistIO' "c7e0a603f3a22c7e85daa4599bf92525"
+    P.putStrLn "Finished downloading 2."
+    threadDelay $ 1000 * 500
+    content <- try $ P.readFile "c7e0a603f3a22c7e85daa4599bf92525"
+    P.putStrLn "File 2 read."
+    pure content
+
+  L.evalIO $ P.putStrLn "Awaiting line'..."
+  line :: String <- L.await line'
+
+  result' <- L.async $ do
+    _ <- L.await eRes1'
+    eRes2 <- L.await eRes2'
+
+    let resLine1' = (either show id) <$> eRes1'
+    let resLine2 = either show id eRes2
+
+    resLine1 <- L.await resLine1'
+
+    pure (line <> " " <> resLine1 <> " " <> resLine2)
+
+  L.evalIO $ P.putStrLn "Some async event."
+
+  result <- L.await result'
+
+  L.evalIO $ P.putStrLn "All data got."
+  L.evalIO $ P.putStrLn result
+  pure ()
+
+sampleApp1 :: L.ZIO ()
+sampleApp1 = void $ L.evalAsyncEffect appLogic
+
+-- Sample 2
 
 gistLengthUnsafe :: L.AsyncEffect (Int, Int)
 gistLengthUnsafe = do
@@ -36,6 +100,12 @@ gistLengthSafe = do
       -> L.evalIO $ P.putStrLn $ "Exception got: " <> show err
     Right val
       -> L.evalIO $ P.putStrLn $ "You got: " <> show val
+
+sampleApp2 :: L.ZIO ()
+sampleApp2 = gistLengthSafe
+
+
+-- Sample 3
 
 fibonacci :: Int -> Int
 fibonacci n = head $ drop n fibs
@@ -68,13 +138,11 @@ asyncApp = do
   fact  <- L.await aFact
   pure (fib, fact)
 
-app :: L.ZIO (Int, Int)
-app = L.evalAsyncEffect asyncApp
+sampleApp3 :: L.ZIO ()
+sampleApp3 = do
+  fibAndFact <- L.evalAsyncEffect asyncApp
+  void $ L.evalIO $ print fibAndFact
 
 main :: IO ()
-main = R.withZIORuntime $ \rt -> do
-  (fib, fact) <- R.runZIO rt app
-  print "You got:"
-  print (fib, fact)
-
-  -- void $ R.runZIO rt gistLengthSafe
+main = R.withZIORuntime $ \rt ->
+  R.runZIO rt sampleApp1
